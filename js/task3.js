bdf5f5deb0e4b439fc18ac41e3707b169868f536
sourav <sauravkumarji99@gmail.com>
2022-06-29 22:24:55 +0530

    assigned value to a

---
+++ js/task3.js
@@ -0,0 +1,9 @@
+// First, this code creates a variable named myVar. Then, the code assigns 5 to myVar. Now, if myVar appears again in the code, the program will treat it as if it is 5.
+
+
+// Assign the value 7 to variable a.
+
+var a;
+a = 7;
+
+
