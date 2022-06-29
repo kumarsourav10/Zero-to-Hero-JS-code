506fe48579b234b62f440e7e5614dd9ce20fcb16
sourav <sauravkumarji99@gmail.com>
2022-06-29 22:54:52 +0530

    decleared variables

---
+++ js/task6.js
@@ -0,0 +1,9 @@
+// Create two new string variables: myFirstName and myLastName and assign them the values of your first and last name, respectively.
+
+
+// myFirstName should be a string with at least one character in it.
+
+// myLastName should be a string with at least one character in it.
+
+var myFirstName = "myFirstName";
+var myLastName = "myLastName";
