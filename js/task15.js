275e0984de6a1947a73464d1d1ebb3cdf8d02d7e
sourav <sauravkumarji99@gmail.com>
2022-06-30 00:48:28 +0530

    precticed increment value ++ system

---
+++ js/task15.js
@@ -0,0 +1,12 @@
+// Change the code to use the ++ operator on myVar.
+
+// myVar should equal 88.
+// Waiting:You should not use the assignment operator.
+// Waiting:You should use the ++ operator.
+// Waiting:You should not change code above the specified comment.
+
+let myVar = 87;
+
+// Only change code below this line
+myVar++;
+ 
