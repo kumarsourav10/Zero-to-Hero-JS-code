0c007c4c8f7230e36956179c2a61bdf0947d63c5
sourav <sauravkumarji99@gmail.com>
2022-06-29 22:37:04 +0530

    give value to a

---
+++ js/task4.js
@@ -0,0 +1,14 @@
+// The above declares a myVar variable with no value, then assigns it the value 5. Next, a variable named myNum is declared with no value. Then, the contents of myVar (which is 5) is assigned to the variable myNum. Now, myNum also has the value of 5.
+
+// Assign the contents of a to variable b.
+
+
+
+// You should not change code above the specified comment.
+// b should have a value of 7.
+// a should be assigned to b with =.
+var a;
+a = 7;
+var b;
+b = 7;
+b = a;
