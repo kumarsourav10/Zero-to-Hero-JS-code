86db12ea162bc908741c820151c5caf0b24fcff0
sourav <sauravkumarji99@gmail.com>
2022-06-29 23:50:06 +0530

    decleared variables

--- js/task7.js
+++ js/task7.js
@@ -6,3 +6,12 @@
 // Waiting:c should not contain undefined and should have a final value of the string I am a String
 // You should not change code below the specified comment.
 
+// Only change code below this line
+var a = 5;
+var b = 10;
+var c = "I am a";
+// Only change code above this line
+
+a = a + 1;
+b = b + 5;
+c = c + " String!";
---
+++ js/task8.js
@@ -0,0 +1,22 @@
+// Modify the existing declarations and assignments so their names use camelCase.
+
+// Do not create any new variables.
+
+// studlyCapVar should be defined and have a value of 10.
+// Passed:properCamelCase should be defined and have a value of the string A String.
+// Passed:titleCaseOver should be defined and have a value of 9000.
+// Passed:studlyCapVar should use camelCase in both declaration and assignment sections.
+// Passed:properCamelCase should use camelCase in both declaration and assignment sections.
+// Passed:titleCaseOver should use camelCase in both declaration and assignment sections.
+
+// Variable declarations
+
+
+var studlyCapVar = 10;
+var properCamelCase = "A String";
+var titleCaseOver = 9000;
+
+// Variable assignments
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
