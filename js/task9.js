0127629f53263077f4a9e9197ca35284bffbd2d0
sourav <sauravkumarji99@gmail.com>
2022-06-29 23:51:34 +0530

    used let instead of var

---
+++ js/task9.js
@@ -0,0 +1,7 @@
+
+// Update the code so it only uses the let keyword.
+
+// Waiting:var should not exist in the code.
+// Waiting:catName should be the string Oliver.
+// Waiting:catSound should be the string Meow!
+
