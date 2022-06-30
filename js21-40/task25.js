02b027e7dbb6c6a1890ba7b2b2ebaa8d7b438d11
sourav <sauravkumarji99@gmail.com>
2022-06-30 16:29:21 +0530

    Compound Assignment With Aug,emted assignments

---
+++ js21-40/task25.js
@@ -0,0 +1,13 @@
+// Use backslashes to assign a string to the myStr variable so that if you were to print it to the console, you would see:
+
+// I am a "double quoted" string inside "double quotes".
+
+// You should use two double quotes (") and four escaped double quotes (\").
+// Passed:Variable myStr should contain the string: I am a "double quoted" string inside "double quotes".
+
+
+
+const myStr ="I am a \"double quoted\" string inside \"double quotes\"."; 
+ // Change this line;
+
+ console.log(myStr);
