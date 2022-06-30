2891bbb46d72d2860a20494977c4ad78257f58e3
sourav <sauravkumarji99@gmail.com>
2022-07-01 00:16:13 +0530

    add the value inside the array

---
+++ js41-60/task42.js
@@ -0,0 +1,12 @@
+// Modify the data stored at index 0 of myArray to a value of 45.
+
+// myArray should now be [45, 64, 99].
+// Waiting:You should be using correct index to modify the value in myArray.
+
+
+// Setup
+const myArray = [18, 64, 99];
+
+// Only change code below this line
+myArray[0] = 45;
+console.log (myArray)
