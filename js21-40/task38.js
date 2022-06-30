a367217349e8fe354e84a9317ae5ec615c7eb298
sourav <sauravkumarji99@gmail.com>
2022-06-30 23:52:48 +0530

    added adjective noun by concatinate

---
+++ js21-40/task38.js
@@ -0,0 +1,23 @@
+// In this challenge, we provide you with a noun, a verb, an adjective and an adverb. You need to form a complete sentence using words of your choice, along with the words we provide.
+
+// You will need to use the string concatenation operator + to build a new string, using the provided variables: myNoun, myAdjective, myVerb, and myAdverb. You will then assign the formed string to the wordBlanks variable. You should not change the words assigned to the variables.
+
+// You will also need to account for spaces in your string, so that the final sentence has spaces between all the words. The result should be a complete sentence.
+
+// wordBlanks should be a string.
+// Waiting:You should not change the values assigned to myNoun, myVerb, myAdjective or myAdverb.
+// Waiting:You should not directly use the values dog, ran, big, or quickly to create wordBlanks.
+// Waiting:wordBlanks should contain all of the words assigned to the variables myNoun, myVerb, myAdjective and myAdverb separated by non-word characters (and any additional words of your choice).
+
+
+const myNoun = "dog";
+const myAdjective = "big";
+const myVerb = "ran";
+const myAdverb = "quickly";
+
+// Only change code below this line
+const wordBlanks = "my " + myAdjective + " " + myNoun + " " + myVerb + " " + myAdverb ;
+; // Change this line
+// Only change code above this line
+
+console.log(wordBlanks)
