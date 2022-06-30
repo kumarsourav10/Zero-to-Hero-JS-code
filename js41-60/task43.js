a6645243c91c63c894f645d861ecd82b384a7f2f
sourav <sauravkumarji99@gmail.com>
2022-07-01 00:30:53 +0530

    get deta from multifunctinol arrays

---
+++ js41-60/task43.js
@@ -0,0 +1,14 @@
+// Using bracket notation select an element from myArray such that myData is equal to 8.
+
+
+// Waiting:myData should be equal to 8.
+// Waiting:You should be using bracket notation to read the correct value from myArray.
+
+const myArray = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+    [[10, 11, 12], 13, 14],
+  ];
+  
+  const myData = myArray[2][1];
