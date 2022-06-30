666fbf6bb9a9c0ff9a3a16973ab3b5451eac4076
sourav <sauravkumarji99@gmail.com>
2022-06-30 17:45:13 +0530

    Escape Sequences in Strings

---
+++ js21-40/task27.js
@@ -0,0 +1,16 @@
+// You will need to use escape sequences to insert special characters correctly. You will also need to follow the spacing as it looks above, with no spaces between escape sequences or words.
+
+// Note: The indentation for SecondLine is achieved with the tab escape character, not spaces.
+
+// Waiting:myStr should not contain any spaces
+// Waiting:myStr should contain the strings FirstLine, SecondLine and ThirdLine (remember case sensitivity)
+// Waiting:FirstLine should be followed by the newline character \n
+// Waiting:myStr should contain a tab character \t which follows a newline character
+// Waiting:SecondLine should be preceded by the backslash character \
+// Waiting:There should be a newline character between SecondLine and ThirdLine
+// Waiting:myStr should only contain characters shown in the instructions
+
+
+const myStr = 'FirstLine\n\t\\SecondLine\nThirdLine';
+
+ // Change this line 
