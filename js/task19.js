072a4af75bdd67402bcf68d913218cce272865b2
sourav <sauravkumarji99@gmail.com>
2022-06-30 12:06:11 +0530

    this is the multiply

---
+++ js/task19.js
@@ -0,0 +1,7 @@
+// Change the 0.0 so that quotient will equal to 2.2.
+
+// The variable quotient should equal 2.2
+// Waiting:You should use the / operator to divide 4.4 by 2
+// Waiting:The quotient variable should only be assigned once
+
+const quotient = 4.4 / 2; // Change this line
