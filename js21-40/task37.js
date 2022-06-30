288821e8b3d3201c446b222d3d6a49b7e1127a85
sourav <sauravkumarji99@gmail.com>
2022-06-30 20:42:34 +0530

    get the second to last value

---
+++ js21-40/task37.js
@@ -0,0 +1,12 @@
+// Use bracket notation to find the second-to-last character in the lastName string.
+
+// Hint: Try looking at the example above if you get stuck.
+
+// Waiting:secondToLastLetterOfLastName should be the letter c.
+// Waiting:You should use .length to get the second last letter.
+
+// Setup
+const lastName = "Lovelace";
+
+// Only change code below this line
+const secondToLastLetterOfLastName = lastName[lastName.length-2]; // Change this line
