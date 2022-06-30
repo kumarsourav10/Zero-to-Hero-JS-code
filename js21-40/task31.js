1500cd8ec8b3b1f2c50feb64819feca277e02a9f
sourav <sauravkumarji99@gmail.com>
2022-06-30 20:04:39 +0530

    learnt how to find the length of the string

---
+++ js21-40/task31.js
@@ -0,0 +1,11 @@
+// Set someAdjective to a string of at least 3 characters and append it to myStr using the += operator.
+
+
+// someAdjective should be set to a string at least 3 characters long.
+// Waiting:You should append someAdjective to myStr using the += operator.
+
+
+// Change code below this line
+const someAdjective = "sourav";
+let myStr = "Learning to code is ";
+ myStr += someAdjective ;
