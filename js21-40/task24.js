298c09093ccfa572ea3ccc0aaefe37330a68ff7a
sourav <sauravkumarji99@gmail.com>
2022-06-30 12:43:01 +0530

    Compound Assignment With Aug,emted assignments

---
+++ js21-40/task24.js
@@ -0,0 +1,28 @@
+// Convert the assignments for a, b, and c to use the /= operator.
+
+// a should equal 4.
+// Passed:b should equal 27.
+// Passed:c should equal 3.
+// Failed:You should use the /= operator for each variable.
+// Passed:You should not modify the code above the specified comment.
+
+// this is the code
+/*
+ let a = 48;
+ let b = 108;
+ let c = 33;
+
+ // Only change code below this line
+ a = a / 12;
+ b = b / 4;
+ c = c / 11;
+ */
+
+ let a = 48;
+ let b = 108;
+ let c = 33;
+ 
+ // Only change code below this line
+  a /= 12;
+  b /= 4;
+  c /= 11;
