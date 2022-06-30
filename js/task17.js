1fb8016f0822ee5f09f27683e6e6f1b763d3863e
sourav <sauravkumarji99@gmail.com>
2022-06-30 12:01:59 +0530

    this is the multiply

---
+++ js/task17.js
@@ -0,0 +1,8 @@
+// Create a variable myDecimal and give it a decimal value with a fractional part (e.g. 5.7).
+
+
+// Waiting:myDecimal should be a number.
+// Waiting:myDecimal should have a decimal point
+
+const myDecimal = 5.7;
+
---
+++ js/task18.js
@@ -0,0 +1,7 @@
+// Change the 0.0 so that product will equal 5.0.
+
+// Waiting:The variable product should equal 5.0.
+// Waiting:You should use the * operator
+
+const multiply = 5.0*1.0;
+const multiply2 = 10.0*2.0;
