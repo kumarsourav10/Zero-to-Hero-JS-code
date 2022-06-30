95b18589f3cfa269bf15d1f27c6b83c8596b4868
sourav <sauravkumarji99@gmail.com>
2022-06-30 20:39:26 +0530

    Use bracket notation to get the last words place value

---
+++ js21-40/task36.js
@@ -0,0 +1,12 @@
+// Use bracket notation to find the last character in the lastName variable.
+
+// Hint: Try looking at the example above if you get stuck.
+
+// Waiting:lastLetterOfLastName should be the letter e.
+// Waiting:You should use .length to get the last letter.
+
+// Setup
+const lastName = "Lovelace";
+
+// Only change code below this line
+const lastLetterOfLastName = lastName[lastName.length-1]; // Change this line
