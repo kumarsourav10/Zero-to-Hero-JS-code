853bea2b0fd37c89742a767ba8abb57633f88260
sourav <sauravkumarji99@gmail.com>
2022-06-30 16:38:34 +0530

    Quoting Strings with Single Quotes

--- js21-40/task26.js
+++ js21-40/task26.js
@@ -6,3 +6,5 @@
 // You should remove all the backslashes (\).
 // Waiting:You should have two single quotes ' and four double quotes ".
 
+const myStr = '<a href="http://www.example.com" target="_blank">Link</a>';
+
