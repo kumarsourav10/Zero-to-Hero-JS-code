917d398c944a809fb18efc3e0501962fafa236ee
sourav <sauravkumarji99@gmail.com>
2022-06-30 12:25:46 +0530

    Compound Assignment With Augmented Addition

---
+++ js21-40/task21.js
@@ -0,0 +1,16 @@
+// Convert the assignments for a, b, and c to use the += operator.
+
+// Passed:a should equal 15.
+// Passed:b should equal 26.
+// Passed:c should equal 19.
+// Passed:You should use the += operator for each variable.
+// Passed:You should not modify the code above the specified comment.
+
+let a = 3;
+let b = 17;
+let c = 12;
+
+// Only change code below this line
+ a += 12;
+ b += 9;
+ c += 7;
