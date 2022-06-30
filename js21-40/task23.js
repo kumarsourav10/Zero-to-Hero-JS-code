e77274176469c2806539b6be072e1a78af519768
sourav <sauravkumarji99@gmail.com>
2022-06-30 12:38:30 +0530

    Compound Assignment With Augmented Addition

---
+++ js21-40/task23.js
@@ -0,0 +1,30 @@
+// Convert the assignments for a, b, and c to use the *= operator.
+
+// a should equal 25.
+// Waiting:b should equal 36.
+// Waiting:c should equal 46.
+// Waiting:You should use the *= operator for each variable.
+// Waiting:You should not modify the code above the specified comment.
+
+// This is the code
+/*
+ let a = 5;
+ let b = 12;
+ let c = 4.6;
+
+ // Only change code below this line
+ a = a * 5;
+ b = 3 * b;
+ c = c * 10;
+*/
+
+let a = 5;
+let b = 12;
+let c = 4.6;
+
+// Only change code below this line
+ a *= 5;
+ b *= 3;
+ c *= 10;
+
+
