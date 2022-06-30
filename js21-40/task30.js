54ff09cc08e0a590ee29b66c99b1f7ac09703f20
sourav <sauravkumarji99@gmail.com>
2022-06-30 19:57:44 +0530

    Concatenating Strings with the Plus Equals Operator

---
+++ js21-40/task30.js
@@ -0,0 +1,9 @@
+// Set myName to a string equal to your name and build myStr with myName between the strings My name is and and I am well!
+
+// myName should be set to a string at least 3 characters long.
+// Waiting:You should use two + operators to build myStr with myName inside it.
+
+
+// Only change code below this line
+const myName = "sourav";
+const myStr = "My name is" + myName + "I am well!";
