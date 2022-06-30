6d4a6270d1dcd14aa6aab1d05275ae929f45e294
sourav <sauravkumarji99@gmail.com>
2022-07-01 00:10:40 +0530

    learn how we get the value from array

---
+++ js41-60/task41.js
@@ -0,0 +1,9 @@
+
+// Create a variable called myData and set it to equal the first value of myArray using bracket notation.
+
+// The variable myData should equal the first value of myArray.
+// Waiting:The data in variable myArray should be accessed using bracket notation.
+
+const myArray = [50, 60, 70];
+const myData = 50 ;
+console.log = myArray[0];
