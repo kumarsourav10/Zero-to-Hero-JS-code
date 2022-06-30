2bd0cf4ffbbe8b1bce409f45ca7a7cdddd0a468f
sourav <sauravkumarji99@gmail.com>
2022-06-30 23:59:09 +0530

    learn arrays

---
+++ js21-40/task39.js
@@ -0,0 +1,6 @@
+// Modify the new array myArray so that it contains both a string and a number (in that order).
+
+// Waiting:myArray should be an array.
+// Waiting:The first item in myArray should be a string.
+// Waiting:The second item in myArray should be a number.
+
