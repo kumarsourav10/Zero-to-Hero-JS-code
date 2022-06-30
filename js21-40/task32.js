63dec5659c0878cd33d48d538a1176fbef7210fe
sourav <sauravkumarji99@gmail.com>
2022-06-30 20:08:44 +0530

    Concatenating Strings with the Plus Equals Operator

---
+++ js21-40/task32.js
@@ -0,0 +1,12 @@
+// Use the .length property to set lastNameLength to the number of characters in lastName.
+
+// Passed:You should not change the variable declarations in the // Setup section.
+// Passed:lastNameLength should be equal to eight.
+// Passed:You should be getting the length of lastName by using .length like this: lastName.length.
+
+// Setup
+let lastNameLength = 0;
+const lastName = "Lovelace";
+
+// Only change code below this line
+lastNameLength = lastName.length;
