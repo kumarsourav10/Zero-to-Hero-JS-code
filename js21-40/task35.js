80610a577051aa4f99985e90b1aa4ec21aae991a
sourav <sauravkumarji99@gmail.com>
2022-06-30 20:36:38 +0530

    used bracket notation to get place value from the string

---
+++ js21-40/task35.js
@@ -0,0 +1,13 @@
+
+// Let's try to set thirdLetterOfLastName to equal the third letter of the lastName variable using bracket notation.
+
+// Hint: Try looking at the example above if you get stuck.
+
+// Waiting:The thirdLetterOfLastName variable should have the value of v.
+// Waiting:You should use bracket notation.
+
+// Setup
+const lastName = "Lovelace";
+
+// Only change code below this line
+const thirdLetterOfLastName = lastName[2]; // Change this line
