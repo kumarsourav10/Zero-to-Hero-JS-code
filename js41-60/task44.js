e396142d765b78590028c79b4d4550aab120d9a1
sourav <sauravkumarji99@gmail.com>
2022-07-01 00:40:15 +0530

    learn how we get the value from array

---
+++ js41-60/task44.js
@@ -0,0 +1,12 @@
+
+// Push ["dog", 3] onto the end of the myArray variable.
+
+// Waiting:myArray should now equal [["John", 23], ["cat", 2], ["dog", 3]].
+
+
+// Setup
+const myArray = [["John", 23], ["cat", 2]];
+
+// Only change code below this line
+
+myArray.push(["dog", 3]);
