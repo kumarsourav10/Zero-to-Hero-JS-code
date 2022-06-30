c5c77be71c2ddfda847833657a833bb0cbce35b5
sourav <sauravkumarji99@gmail.com>
2022-06-30 18:12:45 +0530

    Concatenating Strings with the Plus Equals Operator

---
+++ js21-40/task28.js
@@ -0,0 +1,11 @@
+
+// Build myStr from the strings This is the start. and This is the end. using the + operator. Be sure to include a space between the two strings.
+
+// myStr should have a value of the string This is the start. This is the end.
+// Waiting:You should use the + operator to build myStr.
+// Waiting:myStr should be created using the const keyword.
+// Waiting:You should assign the result to the myStr variable.
+
+const myStr = "This is the start." + " This is the end."; 
+
+// Change this line
