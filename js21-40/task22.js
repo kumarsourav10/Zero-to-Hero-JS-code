1f175bbc7b55bdf607ba4a942c1362d8a1e77d7f
sourav <sauravkumarji99@gmail.com>
2022-06-30 12:32:13 +0530

    Compound Assignment With Augmented Addition

---
+++ js21-40/task22.js
@@ -0,0 +1,30 @@
+// Convert the assignments for a, b, and c to use the -= operator.
+
+// a should equal 5.
+// Waiting:b should equal -6.
+// Waiting:c should equal 2.
+// Waiting:You should use the -= operator for each variable.
+// Waiting:You should not modify the code above the specified comment..
+
+/*
+this is the question
+
+let a = 11;
+let b = 9;
+let c = 3;
+
+// Only change code below this line
+a = a - 6;
+b = b - 15;
+c = c - 1;
+*/
+
+// This is the answer
+let a = 11;
+let b = 9;
+let c = 3;
+
+// Only change code below this line
+ a -= 6;
+ b -= 15;
+c -= 1;
