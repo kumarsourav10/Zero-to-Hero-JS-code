ce219821af8412a71f3ff48e52a259fed3468638
sourav <sauravkumarji99@gmail.com>
2022-06-30 12:13:49 +0530

    this is the multiply

---
+++ js/task20.js
@@ -0,0 +1,7 @@
+// Set remainder equal to the remainder of 11 divided by 3 using the remainder (%) operator.
+
+// The variable remainder should be initialized
+// Waiting:The value of remainder should be 2
+// Waiting:You should use the % operator
+
+const remainder = 11 % 3;
