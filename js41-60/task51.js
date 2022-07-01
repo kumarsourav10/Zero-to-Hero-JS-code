c2e7f46234f1bf260a447a58d23857cd98b221eb
sourav <sauravkumarji99@gmail.com>
2022-07-01 22:55:09 +0530

    learnt how to give a value to a funtion how they return a value back

---
+++ js41-60/task51.js
@@ -0,0 +1,14 @@
+// Create a function timesFive that accepts one argument, multiplies it by 5, and returns the new value.
+
+
+// timesFive should be a function
+// Failed:timesFive(5) should return 25
+// Failed:timesFive(2) should return 10
+// Failed:timesFive(0) should return 0
+
+function timesFive (five) {
+    return five * 5;
+  }
+  timesFive();
+  
+  
