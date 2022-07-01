25927607c8347779dcfdc1225154c02a052e9d8b
sourav <sauravkumarji99@gmail.com>
2022-07-01 19:11:55 +0530

    learnt how to removed from an array used pop

---
+++ js41-60/task45.js
@@ -0,0 +1,14 @@
+// Use the .pop() function to remove the last item from myArray and assign the popped off value to a new variable, removedFromMyArray.
+
+// Waiting:myArray should only contain [["John", 23]].
+// Waiting:You should use pop() on myArray.
+// Waiting:removedFromMyArray should only contain ["cat", 2].
+
+// Setup
+const myArray = [["John", 23], ["cat", 2]];
+
+// Only change code below this line
+const removedFromMyArray = myArray.pop();
+
+console.log (removedFromMyArray)
+
