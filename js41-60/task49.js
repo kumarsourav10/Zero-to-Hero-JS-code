9bbbb1d63088a77843aeacf0da9faf0d7cb9b501
sourav <sauravkumarji99@gmail.com>
2022-07-01 20:21:33 +0530

    learnt how to create a function and give the value

---
+++ js41-60/task49.js
@@ -0,0 +1,13 @@
+// Create a function called reusableFunction which prints the string Hi World to the dev console.
+// Call the function.
+
+// Waiting:reusableFunction should be a function.
+// Waiting:If reusableFunction is called, it should output the string Hi World to the console.
+// Waiting:You should call reusableFunction once it is defined.
+
+
+function reusableFunction() {
+    console.log("Hi World");
+}
+
+reusableFunction();
