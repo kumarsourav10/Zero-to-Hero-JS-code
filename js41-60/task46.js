9a8280c84cae89185fe7df759e65d37d5e9a32b3
sourav <sauravkumarji99@gmail.com>
2022-07-01 19:17:35 +0530

    learnt how to removed from starting

---
+++ js41-60/task46.js
@@ -0,0 +1,14 @@
+// Use the .shift() function to remove the first item from myArray and assign the "shifted off" value to a new variable, removedFromMyArray.
+
+// Waiting:myArray should now equal [["dog", 3]].
+// Waiting:removedFromMyArray should contain ["John", 23].
+
+
+// Setup
+const myArray = [["John", 23], ["dog", 3]];
+
+// Only change code below this line
+
+const removedFromMyArray = myArray.shift();
+
+console.log(myArray);
