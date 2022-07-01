85b8d41590ed5e8a31ba05bbfd9338e3eff2db36
sourav <sauravkumarji99@gmail.com>
2022-07-01 20:02:04 +0530

    learnt how to add sub array in main array

---
+++ js41-60/task48.js
@@ -0,0 +1,12 @@
+// There should be at least 5 sub-arrays in the list.
+
+
+
+
+// Waiting:myList should be an array.
+// Waiting:The first elements in each of your sub-arrays should all be strings.
+// Waiting:The second elements in each of your sub-arrays should all be numbers.
+// Waiting:You should have at least 5 items in your list.
+
+
+const myList = [["School-bag", 5],["Mobile", 1],["Tripod", 2],["Peanut-butter", 9],["fan", 8]];
