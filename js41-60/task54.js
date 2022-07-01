18b477cd6482046177216359d027415793e5afa8
sourav <sauravkumarji99@gmail.com>
2022-07-01 23:55:43 +0530

    learnt global variable or oops variable

---
+++ js41-60/task54.js
@@ -0,0 +1,18 @@
+// Add a local variable to myOutfit function to override the value of outerWear with the string sweater.
+
+// You should not change the value of the global outerWear.
+// Waiting:myOutfit should return the string sweater.
+// Waiting:You should not change the return statement.
+
+
+// Setup
+const outerWear = "T-Shirt";
+
+function myOutfit() {
+  // Only change code below this line
+return myOutfit = "sweater"
+  // Only change code above this line
+  return outerWear;
+}
+
+myOutfit();
