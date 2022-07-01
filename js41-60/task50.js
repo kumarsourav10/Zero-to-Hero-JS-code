3fca24754fd3f09a8beb9bb36cab85a1225c9678
sourav <sauravkumarji99@gmail.com>
2022-07-01 21:04:05 +0530

    learnt that there is two parameters in one function

---
+++ js41-60/task50.js
@@ -0,0 +1,16 @@
+// Create a function called functionWithArgs that accepts two arguments and outputs their sum to the dev console.
+// Call the function with two numbers as arguments.
+
+// Tests
+// functionWithArgs should be a function.
+// functionWithArgs(1,2) should output 3.
+// functionWithArgs(7,9) should output 16.
+// You should call functionWithArgs with two numbers after you define it.
+
+
+function functionWithArgs(one, two) {
+    console.log (one + two);
+ }
+  
+  functionWithArgs(7, 9);
+
