0baab39b1f85b457daa029fd07be5799f441e341
sourav <sauravkumarji99@gmail.com>
2022-07-02 15:31:17 +0530

    learn boolean

---
+++ js41-60/task58.js
@@ -0,0 +1,12 @@
+// Modify the welcomeToBooleans function so that it returns true instead of false when the run button is clicked.
+
+// Waiting:The welcomeToBooleans() function should return a Boolean (true or false) value.
+// Waiting:welcomeToBooleans() should return true.
+
+function welcomeToBooleans() {
+    // Only change code below this line
+  
+    return true; // Change this line
+  
+    // Only change code above this line
+  }
