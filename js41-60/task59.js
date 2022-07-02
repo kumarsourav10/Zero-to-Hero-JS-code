cb1d0813ae1c8157fe0cd552be225b98f42d7ea6
sourav <sauravkumarji99@gmail.com>
2022-07-02 18:12:41 +0530

    learn use conditional logic to use booleans

---
+++ js41-60/task59.js
@@ -0,0 +1,17 @@
+// Create an if statement inside the function to return Yes, that was true if the parameter wasThatTrue is true and return No, that was false otherwise.
+
+// Waiting:trueOrFalse should be a function
+// Waiting:trueOrFalse(true) should return a string
+// Waiting:trueOrFalse(false) should return a string
+// Waiting:trueOrFalse(true) should return the string Yes, that was true
+// Waiting:trueOrFalse(false) should return the string No, that was false
+
+function trueOrFalse(wasThatTrue) {
+    // Only change code below this line
+  if(wasThatTrue) {
+    return "Yes, that was true" ;
+  }
+  return "No, that was false"
+      // Only change code above this line
+    }
+  
