90c119902a2bc641ae5a569023b750baedc1a3fd
sourav <sauravkumarji99@gmail.com>
2022-07-02 15:19:53 +0530

    learn how to stand in line

---
+++ js41-60/task56.js
@@ -0,0 +1,15 @@
+// Call the processArg function with an argument of 7 and assign its return value to the variable processed.
+
+// Passed:processed should have a value of 2
+// Passed:You should assign processArg to processed
+
+// Setup
+// let processed = 0;
+
+function processArg(num) {
+    return (num + 3) / 5;
+  }
+  
+  // Only change code below this line
+  
+  var processed = processArg(7)
