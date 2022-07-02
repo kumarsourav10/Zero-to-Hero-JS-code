774310f921cd8a4fcc509f90812ea3ba9d2e0b84
sourav <sauravkumarji99@gmail.com>
2022-07-02 14:04:59 +0530

    learn how to pass the value without return function

---
+++ js41-60/task55.js
@@ -0,0 +1,23 @@
+// Create a function addFive without any arguments. This function adds 5 to the sum variable, but its returned value is undefined.
+
+//addFive should be a function.
+//Once both functions have run, the sum should be equal to 8.
+//Returned value from addFive should be undefined.
+//Inside the addFive function, you should add 5 to the sum variable.
+
+// Setup
+let sum = 0;
+
+function addThree() {
+  sum = sum + 3;
+}
+
+// Only change code below this line
+function addFive() {
+  sum = sum + 5
+}
+
+// Only change code above this line
+
+addThree();
+addFive();
