a2de7921bdf5c724abddf7cf992b790af11c18fe
sourav <sauravkumarji99@gmail.com>
2022-07-08 19:10:59 +0530

    updating object properties

---
+++ js81-100/task87.js
@@ -0,0 +1,16 @@
+// Update the myDog object's name property. Let's change her name from Coder to Happy Coder. You can use either dot or bracket notation.
+
+// Waiting:You should update myDog's name property to equal the string Happy Coder.
+// Waiting:You should not edit the myDog definition.
+
+// Setup
+const myDog = {
+    "name": "Coder",
+    "legs": 4,
+    "tails": 1,
+    "friends": ["freeCodeCamp Campers"]
+  };
+  
+  // Only change code below this line
+  myDog.name = "Happy Coder"
+  console.log(myDog)
