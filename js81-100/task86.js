47063e40de7c2552e8c83d20b7fca405d639d277
sourav <sauravkumarji99@gmail.com>
2022-07-08 18:01:19 +0530

    learn Accessing Object Properties with Variables

---
+++ js81-100/task86.js
@@ -0,0 +1,21 @@
+
+// Set the playerNumber variable to 16. Then, use the variable to look up the player's name and assign it to player.
+
+// Waiting:playerNumber should be a number
+// Waiting:The variable player should be a string
+// Waiting:The value of player should be the string Montana
+// Waiting:You should use bracket notation to access testObj
+// Waiting:You should not assign the value Montana to the variable player directly.
+// Waiting:You should be using the variable playerNumber in your bracket notation
+
+
+// Setup
+const testObj = {
+    12: "Namath",
+    16: "Montana",
+    19: "Unitas"
+  };
+  
+  // Only change code below this line
+  const playerNumber = 16;  // Change this line
+  const player = testObj[playerNumber];   // Change this line
