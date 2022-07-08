5b56bf7d5eeb7011ce199c98f8850ae5c4005a79
sourav <sauravkumarji99@gmail.com>
2022-07-08 12:49:47 +0530

    learn to Returning Boolean Values from Functions

--- js61-80/task79.js
+++ js61-80/task79.js
@@ -15,23 +15,32 @@ function chainToSwitch(val) {
     let answer = "";
     // Only change code below this line
   
-    switch (val) {
-    case "bob":
-      answer = "Marley";
-      return ;
-      case "42" :
-      answer = "The Answer"
-      return ;
-      case "1" :
-      answer = 'There is no #1'
-      return ;
-      case "99" :
-      answer = "Missed me by this much!"
-      return ;
-      case "7":
-      answer = "Ate Nine"
-      return ;
-  }
+    function chainToSwitch(val) {
+        let answer = "";
+        // Only change code below this line
+      
+        switch (val) {
+        case "bob":
+          answer = "Marley";
+          break ;
+          case 42 :
+          answer = "The Answer" ;
+          break ;
+          case 1 :
+          answer = 'There is no #1' ;
+          break ;
+          case 99 :
+          answer = "Missed me by this much!" ;
+          break ;
+          case 7:
+          answer = "Ate Nine" 
+      }
+      
+        // Only change code above this line
+        return answer;
+      }
+      
+      chainToSwitch(7);
   
     // Only change code above this line
     return answer;
---
+++ js61-80/task80.js
@@ -0,0 +1,13 @@
+// Fix the function isLess to remove the if/else statements.
+
+// isLess(10, 15) should return true
+// Waiting:isLess(15, 10) should return false
+// Waiting:You should not use any if or else statements
+
+function isLess(a, b) {
+    // Only change code below this line
+    return (a < b)
+    // Only change code above this line
+  }
+  
+  isLess(10, 15);
