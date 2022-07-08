58011245666a68a1bebf395e6ebbe87f86b2fc85
sourav <sauravkumarji99@gmail.com>
2022-07-08 16:47:53 +0530

    Accessing Object Properties with Bracket Notation

---
+++ js81-100/task85.js
@@ -0,0 +1,18 @@
+// Read the values of the properties an entree and the drink of testObj using bracket notation and assign them to entreeValue and drinkValue respectively.
+
+// Waiting:entreeValue should be a string
+// Waiting:The value of entreeValue should be the string hamburger
+// Waiting:drinkValue should be a string
+// Waiting:The value of drinkValue should be the string water
+// Waiting:You should use bracket notation twice
+
+// Setup
+const testObj = {
+    "an entree": "hamburger",
+    "my side": "veggies",
+    "the drink": "water"
+  };
+  
+  // Only change code below this line
+  const entreeValue = testObj["an entree"];   // Change this line
+  const drinkValue = testObj["the drink"];    // Change this line
