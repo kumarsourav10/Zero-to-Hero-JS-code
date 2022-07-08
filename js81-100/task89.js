a2400a950dd1008ebbef590ecd93896d08e75e64
sourav <sauravkumarji99@gmail.com>
2022-07-08 19:23:01 +0530

    learn how to delete the properties from a js object

---
+++ js81-100/task89.js
@@ -0,0 +1,17 @@
+// Delete the tails property from myDog. You may use either dot or bracket notation.
+
+// You should delete the property tails from myDog.
+// Waiting:You should not modify the myDog setup.
+
+
+// Setup
+const myDog = {
+    "name": "Happy Coder",
+    "legs": 4,
+    "tails": 1,
+    "friends": ["freeCodeCamp Campers"],
+    "bark": "woof"
+  };
+  
+  // Only change code below this line
+  delete myDog.tails 
