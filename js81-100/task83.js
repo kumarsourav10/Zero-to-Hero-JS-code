7a7797eadb77a1164214ba6588c7b1c040d778e8
sourav <sauravkumarji99@gmail.com>
2022-07-08 15:57:08 +0530

    build js objects

---
+++ js81-100/task83.js
@@ -0,0 +1,21 @@
+// Make an object that represents a dog called myDog which contains the properties name (a string), legs, tails and friends.
+
+// You can set these object properties to whatever values you want, as long as name is a string, legs and tails are numbers, and friends is an array.
+
+
+// myDog should contain the property name and it should be a string.
+// Waiting:myDog should contain the property legs and it should be a number.
+// Waiting:myDog should contain the property tails and it should be a number.
+// Waiting:myDog should contain the property friends and it should be an array.
+// Waiting:myDog should only contain all the given properties.
+
+
+const myDog = {
+    // Only change code below this line
+  "name" : "cow",
+   "legs" : 4,
+  "tails" : 1 ,   
+  "friends" : ["humanbeing", "Dogs"]
+  
+    // Only change code above this line
+  };
