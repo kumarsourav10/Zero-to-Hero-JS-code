e8df9c36bcb3207b9f57fac4af52c622472a0f0b
sourav <sauravkumarji99@gmail.com>
2022-07-08 19:18:42 +0530

    learn how we can add the property

---
+++ js81-100/task88.js
@@ -0,0 +1,15 @@
+// Add a bark property to myDog and set it to a dog sound, such as "woof". You may use either dot or bracket notation.
+
+
+// Waiting:You should add the property bark to myDog.
+// Waiting:You should not add bark to the initialization of myDog.
+
+
+const myDog = {
+    "name": "Happy Coder",
+    "legs": 4,
+    "tails": 1,
+    "friends": ["freeCodeCamp Campers"]
+  };
+  
+  myDog.bark = "woof"
