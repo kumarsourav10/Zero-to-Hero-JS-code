651c27021f606ac7b0675b3a79be193adbf35c99
sourav <sauravkumarji99@gmail.com>
2022-07-09 22:48:09 +0530

    learn Accessing nasted Arrays

---
+++ js81-100/task94.js
@@ -0,0 +1,25 @@
+// Using dot and bracket notation, set the variable secondTree to the second item in the trees list from the myPlants object.
+
+// secondTree should equal the string pine.
+// Your code should use dot and bracket notation to access myPlants.
+
+const myPlants = [
+    {
+      type: "flowers",
+      list: [
+        "rose",
+        "tulip",
+        "dandelion"
+      ]
+    },
+    {
+      type: "trees",
+      list: [
+        "fir",
+        "pine",
+        "birch"
+      ]
+    }
+  ];
+  
+  const secondTree = myPlants[1].list[1];
