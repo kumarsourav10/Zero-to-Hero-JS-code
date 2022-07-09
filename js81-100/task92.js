107e792fa82ccfc7c2365e643fd5f53fc9d6cbc8
sourav <sauravkumarji99@gmail.com>
2022-07-09 18:30:43 +0530

    learn manipulating Complex objects

---
+++ js81-100/task92.js
@@ -0,0 +1,36 @@
+ // Add a new album to the myMusic array. Add artist and title strings, release_year number, and a formats array of strings.
+
+// Waiting:myMusic should be an array
+// Waiting:myMusic should have at least two elements
+// Waiting:The elements in the myMusic array should be objects
+// Waiting:Your object in myMusic should have at least 4 properties
+// Waiting:Your object in myMusic should contain the property artist which is a string
+// Waiting:Your object in myMusic should contain the property title which is a string
+// Waiting:Your object in myMusic should contain the property release_year which is a number
+// Waiting:Your object in myMusic should contain a formats property which is an array
+// Waiting:formats should be an array of strings with at least two elements
+
+const myMusic = [
+    {
+      "artist": "Billy Joel",
+      "title": "Piano Man",
+      "release_year": 1973,
+      "formats": [
+        "CD",
+        "8T",
+        "LP"
+      ],
+      "gold": true
+    },
+    {
+      "artist": "Arijit Singh",
+      "title": "channa mereya",
+      "release_year": 2018,
+      "formats": [
+        "mp3",
+        "3d",
+      ],
+      "gold": true
+    }
+  
+  ];
