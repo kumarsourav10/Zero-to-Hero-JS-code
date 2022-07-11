c2c52b67ac7c3ec3f018ff378d7633d07dea5e41
sourav <sauravkumarji99@gmail.com>
2022-07-11 14:35:26 +0530

    learn decreament with odd numbers

---
+++ js81-100/task99.js
@@ -0,0 +1,17 @@
+// Push the odd numbers from 9 through 1 to myArray using a for loop.
+
+// Tests
+// Waiting:You should be using a for loop for this.
+// Waiting:You should be using the array method push.
+// Waiting:myArray should equal [9, 7, 5, 3, 1].
+
+// Setup
+const myArray = [];
+
+// Only change code below this line
+let i = 0 
+ for (i = 9 ; i > 0 ; i -= 2) {
+   myArray.push(i)
+ }
+
+ console.log(myArray);
