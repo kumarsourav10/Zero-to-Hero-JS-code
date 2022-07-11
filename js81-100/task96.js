b7038adf258176773b474f6bc0e2681361b97c3e
sourav <sauravkumarji99@gmail.com>
2022-07-11 11:36:14 +0530

    learn while loop

---
+++ js81-100/task96.js
@@ -0,0 +1,16 @@
+
+// Add the numbers 5 through 0 (inclusive) in descending order to myArray using a while loop.
+
+// You should be using a while loop for this.
+// Waiting:myArray should equal [5, 4, 3, 2, 1, 0].
+
+// Setup
+const myArray = [];
+
+// Only change code below this line
+let i = 5;
+
+while (i >= 0) {
+  myArray.push(i)
+  i--
+}
