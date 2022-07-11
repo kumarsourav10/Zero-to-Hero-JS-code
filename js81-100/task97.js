deee743766ee9982fe6ed6c01ef4cf3cc0c453bf
sourav <sauravkumarji99@gmail.com>
2022-07-11 14:16:10 +0530

    learn for loop

---
+++ js81-100/task97.js
@@ -0,0 +1,15 @@
+// Use a for loop to push the values 1 through 5 onto myArray.
+
+// You should be using a for loop for this.
+// Passed:myArray should equal [1, 2, 3, 4, 5].
+
+// Setup
+const myArray = [];
+
+// Only change code below this line
+let i = 1;
+for (let i = 1; i < 6; i++) {
+  myArray.push(i)
+}
+
+console.log(myArray)
