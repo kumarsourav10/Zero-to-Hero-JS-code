40d669adc48c92e33bf9784e187d5b5affcbc3a6
sourav <sauravkumarji99@gmail.com>
2022-07-11 18:59:35 +0530

    learn Generate Random whole nukmbers in js

---
+++ js101-113/task106.js
@@ -0,0 +1,16 @@
+// Generate Random Whole Numbers with JavaScript
+
+
+// Use this technique to generate and return a random whole number between 0 and 9.
+
+// Waiting:The result of randomWholeNum should be a whole number.
+// Waiting:You should use Math.random to generate a random number.
+// Waiting:You should have multiplied the result of Math.random by 10 to make it a number that is between zero and nine.
+// Waiting:You should use Math.floor to remove the decimal part of the number.
+
+function randomWholeNum() {
+
+    // Only change code below this line
+  return Math.floor(Math.random() * 10);
+    // return Math.floor();
+  }
