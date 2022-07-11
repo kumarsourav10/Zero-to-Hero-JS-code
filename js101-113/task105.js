9230ad0e69af98a912e799109c2db66e0fb239f8
sourav <sauravkumarji99@gmail.com>
2022-07-11 18:46:03 +0530

    lean Generate Random Fractions with js

---
+++ js101-113/task105.js
@@ -0,0 +1,15 @@
+// Change randomFraction to return a random number instead of returning 0.
+
+
+// randomFraction should return a random number.
+// Waiting:The number returned by randomFraction should be a decimal.
+// Waiting:You should be using Math.random to generate the random decimal number.
+
+function randomFraction() {
+
+    // Only change code below this line
+  
+    return Math.random();
+  
+    // Only change code above this line
+  }
