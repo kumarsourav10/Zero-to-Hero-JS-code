578bb2a055fd93e77a59940b932b77a159a4bcf5
sourav <sauravkumarji99@gmail.com>
2022-07-11 14:58:01 +0530

    nesting for loops

---
+++ js81-100/task100.js
@@ -0,0 +1,22 @@
+// Declare and initialize a variable total to 0. Use a for loop to add the value of each element of the myArr array to total.
+
+// Waiting:total should be declared and initialized to 0.
+// Waiting:total should equal 20.
+// Waiting:You should use a for loop to iterate through myArr.
+// Waiting:You should not attempt to directly assign the value 20 to total.
+
+// Setup
+const myArr = [2, 3, 4, 5, 6];
+
+// Only change code below this line
+let total = 0
+
+for (let i = 0 ; i < myArr.length ; i++) {
+  total+=myArr[i]
+}
+
+
+// let total = 0;
+// for (let i = 0; i < myArr.length; i++) {
+//   total += myArr[i];
+// }
