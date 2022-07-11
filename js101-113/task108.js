1b73b766e96a9e7a4c2cc749764699d1418aa53d
sourav <sauravkumarji99@gmail.com>
2022-07-11 19:58:10 +0530

    learn the parseInt function

---
+++ js101-113/task108.js
@@ -0,0 +1,17 @@
+// Use parseInt() in the convertToInteger function so it converts the input string str into an integer, and returns it.
+
+// Waiting:convertToInteger should use the parseInt() function
+// Waiting:convertToInteger("56") should return a number
+// Waiting:convertToInteger("56") should return 56
+// Waiting:convertToInteger("77") should return 77
+// Waiting:convertToInteger("JamesBond") should return NaN
+
+
+
+function convertToInteger(str) {
+    return parseInt(str);
+  }
+  
+  
+  
+  convertToInteger("77");
