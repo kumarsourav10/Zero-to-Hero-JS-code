d030acdc4d2d8489b9ee64fb60e245d2d7c05651
sourav <sauravkumarji99@gmail.com>
2022-07-11 18:12:29 +0530

    learn recursive function

---
+++ js101-113/task103.js
@@ -0,0 +1,18 @@
+// Write a recursive function, sum(arr, n), that returns the sum of the first n elements of an array arr.
+
+// sum([1], 0) should equal 0.
+// Waiting:sum([2, 3, 4], 1) should equal 2.
+// Waiting:sum([2, 3, 4, 5], 3) should equal 9.
+// Waiting:Your code should not rely on any kind of loops (for or while or higher order functions such as forEach, map, filter, or reduce.).
+// Waiting:You should use recursion to solve this problem.
+
+function sum(arr, n) {
+    // Only change code below this line
+   if (n <= 0) {
+     return 0 ; 
+   } else {
+     return sum(arr, n-1) + arr[n-1];
+   }
+   
+    // Only change code above this line
+  }
