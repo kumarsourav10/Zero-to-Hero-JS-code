5d1ba65434f3b9b313066026905045148c3f7737
sourav <sauravkumarji99@gmail.com>
2022-07-11 14:28:03 +0530

    learn odd numbers with a for loop

---
+++ js81-100/task98.js
@@ -0,0 +1,13 @@
+// Push the odd numbers from 1 through 9 to myArray using a for loop.
+
+// You should be using a for loop for this.
+// Waiting:myArray should equal [1, 3, 5, 7, 9].
+
+const myArray = [] ;
+
+let i = 1
+for (i = 1 ; i < 10 ; i +=2){
+    myArray.push(i)
+}
+
+console.log(myArray)
