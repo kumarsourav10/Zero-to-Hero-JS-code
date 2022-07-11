702f656d42dfb06625dc8d90dd5c990e71b950a5
sourav <sauravkumarji99@gmail.com>
2022-07-11 17:40:04 +0530

    learn do while loop

---
+++ js101-113/task102.js
@@ -0,0 +1,16 @@
+// Change the while loop in the code to a do...while loop so the loop will push only the number 10 to myArray, and i will be equal to 11 when your code has finished running.
+
+// Passed:You should be using a do...while loop for this exercise.
+// Passed:myArray should equal [10].
+// Passed:i should equal 11
+
+
+// Setup
+const myArray = [];
+let i = 10;
+
+// Only change code below this line
+do{
+  myArray.push(i);
+  i++;
+} while (i < 10) 
