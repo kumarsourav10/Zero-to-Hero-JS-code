3b71f02580083272011562bc850b83f3edff711a
sourav <sauravkumarji99@gmail.com>
2022-07-15 23:38:27 +0530

    I learn to use arrow functions to writw concioua anoonymous functions

---
+++ ES6/test4.js
@@ -0,0 +1,22 @@
+// Use Arrow Functions to Write Concise Anonymous Functions
+
+// Rewrite the function assigned to the variable magic which returns a new Date() to use arrow function syntax. Also, make sure nothing is defined using the keyword var.
+
+
+// Waiting:You should replace the var keyword.
+// Waiting:magic should be a constant variable (by using const).
+// Waiting:magic should be a function.
+// Waiting:magic() should return the correct date.
+// Waiting:The function keyword should not be used.
+
+
+// the previous code
+var magic = function() {
+    return new Date();
+  };
+
+//    the new one 
+
+const magic = () => {
+    return new Date();
+  };
