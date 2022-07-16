396ed21739c4b529012cf521b7e35e199df9f321
sourav <sauravkumarji99@gmail.com>
2022-07-16 17:34:13 +0530

    learn destructuring assignment to assign variable from arrays

---
+++ ES6/test12.js
@@ -0,0 +1,18 @@
+// Use Destructuring Assignment to Assign Variables from Arrays
+
+// Use destructuring assignment to swap the values of a and b so that a receives the value stored in b, and b receives the value stored in a.
+
+// The value of a should be 6, after swapping.
+// Waiting:The value of b should be 8, after swapping.
+// Waiting:You should use array destructuring to swap a and b.
+
+// this is the code 
+// let a = 8, b = 6;
+// Only change code below this line
+
+// this is the code ans.
+
+
+let a = 8, b = 6;
+// Only change code below this line
+[a, b] = [b, a];
