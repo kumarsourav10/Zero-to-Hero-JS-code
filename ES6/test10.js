b859c3b54e1c9840c5c4b041e2a38557b0d535a1
sourav <sauravkumarji99@gmail.com>
2022-07-16 12:22:01 +0530

    learn use of destructing assignment to assign variables from objeccts

---
+++ ES6/test10.js
@@ -0,0 +1,26 @@
+// Use Destructuring Assignment to Assign Variables from Objects
+
+// Replace the two assignments with an equivalent destructuring assignment. It should still assign the variables highToday and highTomorrow the values of today and tomorrow from the HIGH_TEMPERATURES object.
+
+// Waiting:You should remove the ES5 assignment syntax.
+// Waiting:You should use destructuring to create the highToday variable.
+// Waiting:You should use destructuring to create the highTomorrow variable.
+// Waiting:highToday should be equal to 77 and highTomorrow should be equal to 80.
+
+
+// this is the code 
+/*
+   const HIGH_TEMPERATURES = {
+    yesterday: 75,
+    today: 77,
+    tomorrow: 80
+   };
+  
+   // Only change code below this line
+    
+   const highToday = HIGH_TEMPERATURES.today;
+   const highTomorrow = HIGH_TEMPERATURES.tomorrow; 
+  
+   // Only change code above this line
+  
+  */
