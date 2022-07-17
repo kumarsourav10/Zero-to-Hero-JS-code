3f614d5d21851f7ff99248aba37cf62300244ec9
sourav <sauravkumarji99@gmail.com>
2022-07-17 14:09:29 +0530

    learn the use of all export  everyhting from a file

---
+++ ES6/test23.js
@@ -0,0 +1,18 @@
+// Use * to Import Everything from a File
+
+// The code in this file requires the contents of the file: string_functions.js, that is in the same directory as the current file. Use the import * as syntax to import everything from the file into an object called stringFunctions.
+
+// Your code should properly use import * as syntax.
+
+
+// this is the code 
+
+// Only change code above this line
+
+// stringFunctions.uppercaseString("hello");
+// stringFunctions.lowercaseString("WORLD!");
+
+// this is the ans. code
+import * as stringFunctions from "./string_functions.js"
+stringFunctions.uppercaseString("hello");
+stringFunctions.lowercaseString("WORLD!");
