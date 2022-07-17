05828f9ad56333b9b1ae4f9f12ee3af4604f4b10
sourav <sauravkumarji99@gmail.com>
2022-07-17 13:44:00 +0530

    learn to Create a Module Script

---
+++ ES6/test20.js
@@ -0,0 +1,25 @@
+// Create a Module Script
+
+// Add a script to the HTML document of type module and give it the source file of index.js
+
+// Waiting:You should create a script tag.
+// Waiting:Your script tag should have the type attribute with a value of module.
+// Waiting:Your script tag should have a src of index.js.
+
+// thid is code
+{/* <html>
+  <body>
+    <!-- Only change code below this line -->
+
+    <!-- Only change code above this line -->
+  </body>
+</html> */}
+
+// this is the ans. tag
+<html>
+  <body>
+    {/* <!-- Only change code below this line --> */}
+<script type="module" src="index.js"></script>
+    {/* <!-- Only change code above this line --> */}
+  </body>
+</html>
