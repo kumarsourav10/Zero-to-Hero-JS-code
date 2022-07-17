6f88a7598ba337c008d9a976c1a2aa75bf60feb4
sourav <sauravkumarji99@gmail.com>
2022-07-17 14:21:44 +0530

    learn create a javascript Promise

---
+++ ES6/test26.js
@@ -0,0 +1,16 @@
+// Create a JavaScript Promise
+
+// Create a new promise called makeServerRequest. Pass in a function with resolve and reject parameters to the constructor.
+
+
+// Waiting:You should assign a promise to a declared variable named makeServerRequest.
+// Waiting:Your promise should receive a function with resolve and reject as parameters.
+
+
+
+// code is not given in this question  do by yourself
+
+// this is the ans. code
+const makeServerRequest = new Promise((resolve, reject) => {
+
+});
