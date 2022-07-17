de5210e6e162dfdc6d15f9647d771c2df1c7457c
sourav <sauravkumarji99@gmail.com>
2022-07-17 14:17:57 +0530

    learn the use of Import a default export

---
+++ ES6/test25.js
@@ -0,0 +1,17 @@
+// Import a Default Export
+
+// In the following code, import the default export from the math_functions.js file, found in the same directory as this file. Give the import the name subtract.
+
+// You should properly import subtract from math_functions.js.
+
+// this is the code
+  
+// Only change code above this line
+
+// subtract(7,4);
+
+// this is the ans. code
+import subtract from "./math_functions.js"
+// Only change code above this line
+
+subtract(7,4);
