d4b87d05aa08bf578b763fd0b83c3ee2e6859a6d
sourav <sauravkumarji99@gmail.com>
2022-07-17 13:51:05 +0530

    learn to use export to share a code block

---
+++ ES6/test21.js
@@ -0,0 +1,30 @@
+// Use export to Share a Code Block
+
+
+
+// There are two string-related functions in the editor. Export both of them using the method of your choice.
+
+// Waiting:You should properly export uppercaseString.
+// Waiting:You should properly export lowercaseString.
+
+// this is the code
+// const uppercaseString = (string) => {
+//     return string.toUpperCase();
+//   }
+  
+//   const lowercaseString = (string) => {
+//     return string.toLowerCase()
+//   }
+
+
+// this is the ans. code
+const uppercaseString = (string) => {
+    return string.toUpperCase();
+  }
+   export {uppercaseString}
+  
+  const lowercaseString = (string) => {
+    return string.toLowerCase()
+  }
+   export {lowercaseString}
+  
