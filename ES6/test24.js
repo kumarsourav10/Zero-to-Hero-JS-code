82737717995c65e436fcab42e55e252ad85c3f5c
sourav <sauravkumarji99@gmail.com>
2022-07-17 14:14:17 +0530

    learn create an export fallback with export dafault

---
+++ ES6/test24.js
@@ -0,0 +1,17 @@
+// Create an Export Fallback with export default
+
+
+// The following function should be the fallback value for the module. Please add the necessary code to do so.
+
+// Your code should use an export fallback.
+
+// this is the code
+// function subtract(x, y) {
+//     return x - y;
+//   }
+
+// this is the ans. code
+export  default function subtract(x, y) {
+    return x - y;
+  }
+  
