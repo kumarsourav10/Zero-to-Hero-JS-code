f088c327ebe4bab3993fca3a2ee466fa630c2ffb
sourav <sauravkumarji99@gmail.com>
2022-07-18 23:49:17 +0530

    learn using the test Method

---
+++ Ragular_expression/tes1.js
@@ -0,0 +1,18 @@
+// Using the Test Method
+
+// Apply the regex myRegex on the string myString using the .test() method.
+
+
+// Passed:You should use .test() to test the regex.
+// Passed:Your result should return true.
+
+// this is the code
+// let myString = "Hello, World!";
+// let myRegex = /Hello/;
+// let result = myRegex; // Change this line
+
+// this is the ans code
+let myString = "Hello, World!";
+let myRegex = /Hello/;
+let result = myRegex.test(myString); // Change this line
+
