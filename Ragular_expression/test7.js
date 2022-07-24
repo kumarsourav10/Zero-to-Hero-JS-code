cb11e5b072f386af069cb7685edaa2d25bde87b0
sourav <sauravkumarji99@gmail.com>
2022-07-24 21:45:22 +0530

    learn Match Anything with wildcard period

---
+++ Ragular_expression/test7.js
@@ -0,0 +1,28 @@
+// Match Anything with Wildcard Period
+
+
+// Complete the regex unRegex so that it matches the strings run, sun, fun, pun, nun, and bun. Your regex should use the wildcard character.
+
+
+// Waiting:You should use the .test() method.
+// Waiting:You should use the wildcard character in your regex unRegex
+// Waiting:Your regex unRegex should match run in the string Let us go on a run.
+// Waiting:Your regex unRegex should match sun in the string The sun is out today.
+// Waiting:Your regex unRegex should match fun in the string Coding is a lot of fun.
+// Waiting:Your regex unRegex should match pun in the string Seven days without a pun makes one weak.
+// Waiting:Your regex unRegex should match nun in the string One takes a vow to be a nun.
+// Waiting:Your regex unRegex should match bun in the string She got fired from the hot dog stand for putting her hair in a bun.
+// Waiting:Your regex unRegex should not match the string There is a bug in my code.
+// Waiting:Your regex unRegex should not match the string Catch me if you can.
+
+// this is the code
+/*
+let exampleStr = "Let's have fun with regular expressions!";
+let unRegex = /change/; // Change this line
+let result = unRegex.test(exampleStr);
+*/
+
+// this is the code ans.
+let exampleStr = "Let's have fun with regular expressions!";
+let unRegex = /un./; // Change this line
+let result = unRegex.test(exampleStr);
