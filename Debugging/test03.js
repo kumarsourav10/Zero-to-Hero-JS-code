3f92af179984235860bccc86faa41177a3215326
sourav <sauravkumarji99@gmail.com>
2022-08-08 23:57:06 +0530

    learn Use typeof to Check the Type of a Variable

---
+++ Debugging/test03.js
@@ -0,0 +1,24 @@
+// Use typeof to Check the Type of a Variable
+
+// Add two console.log() statements to check the typeof each of the two variables seven and three in the code.
+
+
+// Waiting:Your code should use typeof in two console.log() statements to check the type of the variables.
+// Waiting:Your code should use typeof to check the type of the variable seven.
+// Waiting:Your code should use typeof to check the type of the variable three.
+
+// this is the code
+/*
+let seven = 7;
+let three = "3";
+console.log(seven + three);
+// Only change code below this line
+*/
+
+// this is the code ans
+let seven = 7;
+let three = "3";
+console.log(seven + three);
+// Only change code below this line
+console.log(typeof seven)
+console.log(typeof three)
