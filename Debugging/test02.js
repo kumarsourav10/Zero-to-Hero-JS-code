78632bef09a980d4f1b928ac9afc6ea08c1da038
sourav <sauravkumarji99@gmail.com>
2022-08-08 23:43:29 +0530

    learn Understanding the Differences between the freeCodeCamp and Browser Console

---
+++ Debugging/test02.js
@@ -0,0 +1,23 @@
+
+// Understanding the Differences between the freeCodeCamp and Browser Console
+
+// First, open your browser console so you can see the logs. To do that, you can right-click the freeCodeCamp navigation bar at the top and click inspect on most browsers. Then find the console tab in the window that opens.
+
+// After that, use console.log to log the output variable. View the two consoles to see the log. Finally, use console.clear after your log to clear the browser console. View the difference in the two consoles.
+
+
+
+
+// Waiting:You should use console.log() to print the output variable.
+// Waiting:You should use console.clear() to clear the browser console.
+// Waiting:You should clear the console after your log.
+
+//  this is the code 
+/*
+let output = "Get this to show once in the freeCodeCamp console and not at all in the browser console";
+**/
+
+// this is the code ans.
+let output = "Get this to show once in the freeCodeCamp console and not at all in the browser console";
+console.log(output)
+console.clear()
