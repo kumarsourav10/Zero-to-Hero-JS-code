2960839635a69008e731cf57ad4ed85ae4b7b135
sourav <sauravkumarji99@gmail.com>
2022-08-08 23:20:46 +0530

    learn of use the javascript console to check the value of a variable

---
+++ Debugging/test01.js
@@ -0,0 +1,27 @@
+// Use the JavaScript Console to Check the Value of a Variable
+
+// Use the console.log() method to print the value of the variable a where noted in the code.
+
+// Waiting:Your code should use console.log() to check the value of the variable a.
+
+// this is the code
+/*
+let a = 5;
+let b = 1;
+a++;
+// Only change code below this line
+
+
+let sumAB = a + b;
+console.log(sumAB);
+*/
+
+// this is the code ans
+let a = 5;
+let b = 1;
+a++;
+// Only change code below this line
+
+
+let sumAB = a + b;
+console.log(a);
