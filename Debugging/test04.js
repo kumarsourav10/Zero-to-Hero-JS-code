2f40cbc0f13f76126b32e034d93a7fab3b378bec
sourav <sauravkumarji99@gmail.com>
2022-08-09 00:09:58 +0530

    learn Catch Misspelled Variable and Function Names

---
+++ Debugging/test04.js
@@ -0,0 +1,26 @@
+// Catch Misspelled Variable and Function Names
+
+// Fix the two spelling errors in the code so the netWorkingCapital calculation works.
+
+// Check the spelling of the two variables used in the netWorkingCapital calculation, the console output should show that "Net working capital is: 2".
+// Waiting:There should be no instances of mis-spelled variables in the code.
+// Waiting:The receivables variable should be declared and used properly in the code.
+// Waiting:There should be no instances of mis-spelled variables in the code.
+// Waiting:The payables variable should be declared and used properly in the code.
+
+// this is the code 
+/*
+let receivables = 10;
+let payables = 8;
+let netWorkingCapital = recievables - payable;
+console.log(`Net working capital is: ${netWorkingCapital}`);
+*/
+
+// this is the code ans
+let receivables = 10;
+let payables = 8;
+let netWorkingCapital = receivables - payables;
+console.log(`Net working capital is: ${netWorkingCapital}`);
+
+
+// just you have to check the spell
