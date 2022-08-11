7dbfd3aac5448d8f14b745aeea0e31b4748efcc1
sourav <sauravkumarji99@gmail.com>
2022-08-11 23:11:41 +0530

    learn Catch Unclosed Parantheses Brackets Braces and Quotes

---
+++ Debugging/test05.js
@@ -0,0 +1,18 @@
+// Catch Unclosed Parentheses, Brackets, Braces and Quotes
+
+// Fix the two pair errors in the code.
+
+// Waiting:Your code should fix the missing piece of the array.
+// Waiting:Your code should fix the missing piece of the .reduce() method. The console output should show that Sum of array values is: 6.
+
+// This is the code 
+/*
+let myArray = [1, 2, 3;
+let arraySum = myArray.reduce((previous, current =>  previous + current);
+console.log(`Sum of array values is: ${arraySum}`);
+*/
+
+// This is the code ans.
+let myArray = [1, 2, 3];
+let arraySum = myArray.reduce((previous, current) =>  previous + current);
+console.log(`Sum of array values is: ${arraySum}`);
