eb222f61d9f729d1156a93af5858e1b1ccc4fa4b
sourav <sauravkumarji99@gmail.com>
2022-08-12 18:39:38 +0530

    Learn Prevent Infinite Loops with a Valid Terminal Condition

---
+++ Debugging/test12.js
@@ -0,0 +1,24 @@
+// Prevent Infinite Loops with a Valid Terminal Condition
+
+
+// The myFunc() function contains an infinite loop because the terminal condition i != 4 will never evaluate to false (and break the looping) - i will increment by 2 each pass, and jump right over 4 since i is odd to start. Fix the comparison operator in the terminal condition so the loop only runs for i less than or equal to 4.
+
+
+// Waiting:Your code should change the comparison operator in the terminal condition (the middle part) of the for loop.
+// Waiting:Your code should fix the comparison operator in the terminal condition of the loop.
+
+// This is the code 
+/*
+function myFunc() {
+  for (let i = 1; i != 4; i += 2) {
+    console.log("Still going!");
+  }
+}
+*/
+
+// This is the code ans . 
+function myFunc() {
+    for (let i = 1; i <= 4; i += 2) {
+      console.log("Still going!");
+    }
+  }
