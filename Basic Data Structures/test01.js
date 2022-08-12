41165770f96ed8feb2b0bafccd1c8c8a7dd8a7ce
sourav <sauravkumarji99@gmail.com>
2022-08-13 00:13:05 +0530

    Use an array to store a collection of data

---
+++ Basic Data Structures/test01.js
@@ -0,0 +1,20 @@
+// Use an Array to Store a Collection of Data
+
+
+// We have defined a variable called yourArray. Complete the statement by assigning an array of at least 5 elements in length to the yourArray variable. Your array should contain at least one string, one number, and one boolean.
+
+// yourArray should be an array.
+// Waiting:yourArray should be at least 5 elements long.
+// Waiting:yourArray should contain at least one boolean.
+// Waiting:yourArray should contain at least one number.
+// Waiting:yourArray should contain at least one string.
+
+// This is the code 
+/*
+let yourArray; // Change this line
+*/
+
+// This is the code ans
+
+let yourArray = ["one", 5 , "sourav", "undefined", true] // Change this line
+console.log(yourArray.length)
