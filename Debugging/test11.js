f9a62d0e70bdabc394fa7457df993e1a25013aa0
sourav <sauravkumarji99@gmail.com>
2022-08-12 18:26:58 +0530

    Learn Use Caution When Reinitializing Variables Inside a Loop

---
+++ Debugging/test11.js
@@ -0,0 +1,48 @@
+// Use Caution When Reinitializing Variables Inside a Loop
+
+// The following function is supposed to create a two-dimensional array with m rows and n columns of zeroes. Unfortunately, it's not producing the expected output because the row variable isn't being reinitialized (set back to an empty array) in the outer loop. Fix the code so it returns a correct 3x2 array of zeroes, which looks like [[0, 0], [0, 0], [0, 0]].
+
+// Your code should set the matrix variable to an array holding 3 rows of 2 columns of zeroes each.
+// Waiting:The matrix variable should have 3 rows.
+// Waiting:The matrix variable should have 2 columns in each row.
+// Waiting:zeroArray(4,3) should return an array holding 4 rows of 3 columns of zeroes each.
+
+// This is the code
+/*
+function zeroArray(m, n) {
+  // Creates a 2-D array with m rows and n columns of zeroes
+  let newArray = [];
+  let row = [];
+  for (let i = 0; i < m; i++) {
+    // Adds the m-th row into newArray
+
+    for (let j = 0; j < n; j++) {
+      // Pushes n zeroes into the current row to create the columns
+      row.push(0);
+    }
+    // Pushes the current row, which now has n zeroes in it, to the array
+    newArray.push(row);
+  }
+  return newArray;
+}
+
+let matrix = zeroArray(3, 2);
+console.log(matrix);
+*/
+
+// This is the code ans . 
+function zeroArray(m, n) {
+    let newArray = [];
+    for (let i = 0; i < m; i++) {
+    let row = [];/* <-----  row has been declared inside the outer loop. 
+    Now a new row will be initialised during each iteration of the outer loop allowing 
+    for the desired matrix. */
+      for (let j = 0; j < n; j++) {
+        row.push(0);
+      }
+      newArray.push(row);
+    }
+    return newArray;
+  }
+  let matrix = zeroArray(3, 2);
+  console.log(matrix);
