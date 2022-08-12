38f64d21b02e527f655006f9df3fe7b9bb2ae8ec
sourav <sauravkumarji99@gmail.com>
2022-08-13 00:18:48 +0530

    learn how to Access an array's Contents Using Brackets Notation

---
+++ Basic Data Structures/test02.js
@@ -0,0 +1,26 @@
+// Access an Array's Contents Using Bracket Notation
+
+
+
+// In order to complete this challenge, set the 2nd position (index 1) of myArray to anything you want, besides the letter b.
+
+// Waiting:myArray[0] should be equal to the letter a
+// Waiting:myArray[1] should not be equal to the letter b
+// Waiting:myArray[2] should be equal to the letter c
+// Waiting:myArray[3] should be equal to the letter d
+
+// This is the code 
+/*
+let myArray = ["a", "b", "c", "d"];
+// Only change code below this line
+
+// Only change code above this line
+console.log(myArray);
+*/
+
+// This is the code ans
+let myArray = ["a", "b", "c", "d"];
+// Only change code below this line
+myArray[1] = "This is me capital B"
+// Only change code above this line
+console.log(myArray);
