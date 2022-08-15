df4b071a5d7e6c6ce7d64aa930931bfb5413eeaa
sourav <sauravkumarji99@gmail.com>
2022-08-15 23:04:27 +0530

    learn to Remove Items Using splice

---
+++ Basic Data Structures/test05.js
@@ -0,0 +1,26 @@
+// Remove Items Using splice()
+
+
+// We've initialized an array arr. Use splice() to remove elements from arr, so that it only contains elements that sum to the value of 10.
+
+// Waiting:You should not change the original line of const arr = [2, 4, 5, 1, 7, 5, 2, 1];.
+// Waiting:arr should only contain elements that sum to 10.
+// Waiting:Your code should utilize the splice() method on arr.
+// Waiting:The splice should only remove elements from arr and not add any additional elements to arr.
+
+
+// this is the code 
+/*
+const arr = [2, 4, 5, 1, 7, 5, 2, 1];
+// Only change code below this line
+
+// Only change code above this line
+console.log(arr);
+*/
+
+// This is the code ans
+const arr = [2, 4, 5, 1, 7, 5, 2, 1];
+// Only change code below this line
+ arr.splice(1,4)
+// Only change code above this line
+console.log(arr);
