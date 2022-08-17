13afb877a1f2b970ccc3ead5b83a55f1f976e41b
sourav <sauravkumarji99@gmail.com>
2022-08-17 15:20:07 +0530

    learnt Iterate Through All an Array's Items Using For Loops

---
+++ Basic Data Structures/test13.js
@@ -0,0 +1,42 @@
+// Add Key-Value Pairs to JavaScript Objects
+
+// A foods object has been created with three entries. Using the syntax of your choice, add three more entries to it: bananas with a value of 13, grapes with a value of 35, and strawberries with a value of 27.
+
+
+// foods should be an object.
+// Waiting:The foods object should have a key bananas with a value of 13.
+// Waiting:The foods object should have a key grapes with a value of 35.
+// Waiting:The foods object should have a key strawberries with a value of 27.
+// Waiting:The key-value pairs should be set using dot or bracket notation.
+
+// This is the code 
+/*
+
+let foods = {
+  apples: 25,
+  oranges: 32,
+  plums: 28
+};
+
+// Only change code below this line
+
+// Only change code above this line
+
+console.log(foods);
+
+*/
+
+// This is the code ans 
+let foods = {
+    apples: 25,
+    oranges: 32,
+    plums: 28
+  };
+  
+  // Only change code below this line
+  foods.bananas = 13;
+  foods.grapes  = 35;
+  foods.strawberries = 27;
+  // Only change code above this line
+  
+  console.log(foods);
