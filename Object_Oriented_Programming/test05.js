ef3d20b85b2f9289d0f083c7657db23b863367fb
sourav <sauravkumarji99@gmail.com>
2022-09-03 17:59:36 +0530

    learn Definr a Constructor Function

---
+++ Object_Oriented_Programming/test05.js
@@ -0,0 +1,23 @@
+// Define a Constructor Function
+
+
+// Constructors are functions that create new objects. They define properties and behaviors that will belong to the new object. Think of them as a blueprint for the creation of new objects.
+
+// Here is an example of a constructor:
+
+//                                                               Tests
+// Waiting:Dog should have a name property set to a string.
+// Waiting:Dog should have a color property set to a string.
+// Waiting:Dog should have a numLegs property set to a number.
+
+// There is no code for help just do it by yourself
+
+
+// Answer The Question
+
+function Dog () 
+{ 
+   this.name = "Tommy",
+   this.color = "Black",
+   this.numLegs = 4
+}
