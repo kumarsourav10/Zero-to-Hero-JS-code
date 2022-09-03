32eb6ca1e20a0d527941bea2ed6edae57edd8a3b
sourav <sauravkumarji99@gmail.com>
2022-09-03 17:23:55 +0530

    learn Dot Notation to Access the Properties of an Object

---
+++ Object_Oriented_Programming/test02.js
@@ -0,0 +1,32 @@
+// Use Dot Notation to Access the Properties of an Object
+
+
+// The last challenge created an object with various properties. Now you'll see how to access the values of those properties. Here's an example:
+
+//                                                                Tests 
+// Waiting:Your code should use console.log to print the value for the name property of the dog object.
+// Waiting:Your code should use console.log to print the value for the numLegs property of the dog object.
+
+// This is the code 
+
+/*
+
+let dog = {
+  name: "Spot",
+  numLegs: 4
+};
+// Only change code below this line
+
+*/
+
+
+// This is the code ans
+
+let dog = {
+    name: "Spot",
+    numLegs: 4
+  };
+  // Only change code below this line
+  
+  console.log(dog.name)
+  console.log(dog.numLegs)
