bc8de5daf2cb39a3899b135f0f67b2c3ee1d939e
sourav <sauravkumarji99@gmail.com>
2022-09-03 17:18:38 +0530

    learn Create a Basic JavaScript Object

---
+++ Object_Oriented_Programming/test01.js
@@ -0,0 +1,38 @@
+// # Create a Basic JavaScript Object
+
+// Think about things people see every day, like cars, shops, and birds. These are all objects: tangible things people can observe and interact with.
+
+// What are some qualities of these objects? A car has wheels. Shops sell items. Birds have wings.
+
+// These qualities, or properties, define what makes up an object. Note that similar objects share the same properties, but may have different values for those properties. For example, all cars have wheels, but not all cars have the same number of wheels.
+
+// Objects in JavaScript are used to model real-world objects, giving them properties and behavior just like their real-world counterparts. Here's an example using these concepts to create a duck object:
+
+//                                                                    Tests
+// Waiting:dog should be an object.
+// Waiting:dog should have a name property set to a string.
+// Waiting:dog should have a numLegs property set to a number.
+
+// This is the code 
+
+/*
+
+let dog = {
+
+};
+
+*/
+
+// This  is the ans 
+
+let dog = {
+    name: "Tommy",
+    numLegs : 6 
+};
+
+let Elephant = {
+
+    Color : "Gray" ,
+    legs : "4" ,
+
+}
