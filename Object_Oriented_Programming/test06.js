f926df4e3798170ac747860e121e04511b660258
sourav <sauravkumarji99@gmail.com>
2022-09-03 18:05:54 +0530

    learnUse a Constructor to Create Objects

---
+++ Object_Oriented_Programming/test06.js
@@ -0,0 +1,37 @@
+// // Use a Constructor to Create Objects
+
+
+
+// Use the Dog constructor from the last lesson to create a new instance of Dog, assigning it to a variable hound.
+
+
+
+//                                                                  Tests
+// Failed:hound should be created using the Dog constructor.
+// Failed:Your code should use the new operator to create an instance of Dog.
+
+
+// This is the code 
+
+/*
+
+function Dog() {
+  this.name = "Rupert";
+  this.color = "brown";
+  this.numLegs = 4;
+}
+// Only change code below this line
+
+*/
+
+
+// This is the code ans
+
+function Dog() {
+    this.name = "Rupert";
+    this.color = "brown";
+    this.numLegs = 4;
+  }
+  // Only change code below this line
+  
+  let hound = new Dog(); 
