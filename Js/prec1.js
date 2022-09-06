2f083b59ed0400b449f0d05dd7b82c967fbcce61
sourav <sauravkumarji99@gmail.com>
2022-09-06 13:42:12 +0530

    var let const learnt

---
+++ Js/prec1.js
@@ -0,0 +1,47 @@
+console.log("This repl contains code for the Ultimate Javascript course video no 2 ")
+
+
+let a = 65  //  a contains 65
+console.log(a)
+
+a = "harry" 
+console.log(a)
+
+// let 8harry = 7 // Not allowed this will thore an error
+
+
+// Jawascript is a case sensetive Harry , harry These both are diffrent vlaues
+
+
+// var is global datatype
+ 
+var an = "Sourav"
+
+{
+    var an = "Pandit"
+    console.log(an)
+}
+console.log(an) 
+
+// now you can get to console's one is Sourav the second is pandit but the matter is when you change the calue pandit to sourav that time you got the console sourav sourav 
+
+
+let nam = "Scorpio"
+
+{
+    let nam = "Pajero"
+    console.log(nam)
+}
+
+console.log(nam)
+
+// In this case you cna get two diffrenet answers one is Scorpio and the second is pajero
+
+
+// let's talk about const
+
+const suraj = "mathur"
+ suraj = "uncle"
+console.log(suraj)
+
+// once you set the value to const you can not overRight 
