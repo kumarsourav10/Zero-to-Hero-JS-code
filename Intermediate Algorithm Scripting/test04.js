6328320f7051620c5798aecf039f358085501115
sourav <sauravkumarji99@gmail.com>
2022-09-19 23:09:16 +0530

    learn Wherefore art thou

---
+++ Intermediate Algorithm Scripting/test04.js
@@ -0,0 +1,61 @@
+// Wherefore art thou
+
+
+// Make a function that looks through an array of objects (first argument) and returns an array of all objects that have matching name and value pairs (second argument). Each name and value pair of the source object has to be present in the object from the collection if it is to be included in the returned array.
+
+// For example, if the first argument is [{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], and the second argument is { last: "Capulet" }, then you must return the third object from the array (the first argument), because it contains the name and its value, that was passed on as the second argument.
+
+
+
+
+//                                                                            Tests
+
+
+// Waiting:whatIsInAName([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], { last: "Capulet" }) should return [{ first: "Tybalt", last: "Capulet" }].
+// Waiting:whatIsInAName([{ "apple": 1 }, { "apple": 1 }, { "apple": 1, "bat": 2 }], { "apple": 1 }) should return [{ "apple": 1 }, { "apple": 1 }, { "apple": 1, "bat": 2 }].
+// Waiting:whatIsInAName([{ "apple": 1, "bat": 2 }, { "bat": 2 }, { "apple": 1, "bat": 2, "cookie": 2 }], { "apple": 1, "bat": 2 }) should return [{ "apple": 1, "bat": 2 }, { "apple": 1, "bat": 2, "cookie": 2 }].
+// Waiting:whatIsInAName([{ "apple": 1, "bat": 2 }, { "apple": 1 }, { "apple": 1, "bat": 2, "cookie": 2 }], { "apple": 1, "cookie": 2 }) should return [{ "apple": 1, "bat": 2, "cookie": 2 }].
+// Waiting:whatIsInAName([{ "apple": 1, "bat": 2 }, { "apple": 1 }, { "apple": 1, "bat": 2, "cookie": 2 }, { "bat":2 }], { "apple": 1, "bat": 2 }) should return [{ "apple": 1, "bat": 2 }, { "apple": 1, "bat": 2, "cookie":2 }].
+// Waiting:whatIsInAName([{"a": 1, "b": 2, "c": 3}], {"a": 1, "b": 9999, "c": 3}) should return []
+// Waiting:whatIsInAName([{"a": 1, "b": 2, "c": 3, "d": 9999}], {"a": 1, "b": 9999, "c": 3}) should return []
+
+
+
+// This is the code
+
+/*
+
+function whatIsInAName(collection, source) {
+  var arr = [];
+  
+  for(let i =0; i<collection.length; i++){
+   let objectKeys = Object.keys(collection[i]);
+   for(let j =0; j<objectKeys.length; j++){
+   if (collection[i] [objectKeys[j]] == source[objectKeys[j]]){
+      arr.push(collection[i])
+     }
+    }
+   }
+   console.log(arr);
+   return arr;
+}
+
+
+whatIsInAName([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], { last: "Capulet" });
+
+*/
+
+// This is the code ans
+
+function whatIsInAName(collection, source) {
+   var arr = [];
+   
+  const souceKeys = Object.keys(source);
+ 
+  return collection.filter (obj => souceKeys
+  .map (key => obj.hasOwnProperty(key) && obj[key] === source[key])
+  .reduce((a, b) => a && b));
+ }
+ 
+ 
+ console.log(whatIsInAName([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], { last: "Capulet" }));
