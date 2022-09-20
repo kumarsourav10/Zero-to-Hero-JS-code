748084397bb104e92ecb022e30a6794204db8924
sourav <sauravkumarji99@gmail.com>
2022-09-20 20:59:52 +0530

    This is all basic code for starting JavaScript Code

---
+++ Ultimate_JS_codes/code01.js
@@ -0,0 +1,22 @@
+// This is the very basic code for JavaScript This will print Hello World 
+
+console.log("Hello World")
+
+
+// now I will add 2 numbers 
+
+
+
+let a = 56;
+    b = 75;
+
+    console.log(a + b);
+
+
+    function addThreeNumber  (c, d, e) {
+       return  c + d + e
+    }
+
+    let c =  addThreeNumber(75, 85, 40)
+
+    console.log(c)
