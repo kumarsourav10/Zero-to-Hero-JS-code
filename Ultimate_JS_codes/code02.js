07b6bbdf5a88d81e08ed868baf2bdc03beca8680
sourav <sauravkumarji99@gmail.com>
2022-09-20 21:18:11 +0530

    learn  about value and identifier

---
+++ Ultimate_JS_codes/code02.js
@@ -0,0 +1,38 @@
+const { error } = require("console");
+
+let a = 67 // a Contains 67
+console.log(a)
+a = "Sourav"
+console.log(a)
+
+// in the Javascript you can set the value twice thrice like i set the value to a twice but it will overright the content to newest one .
+
+
+//let 8Sourav = 7;  
+
+// This is not the right way to set the value JavaScript will never allow you to start identifier value by integer like 8 or number a
+
+// and you can not use reserved word to set the value to dentifier like 
+
+//  let var = 10
+
+// let var and many more words are reserved you can google it for your knowledge
+
+// JavaSrcipt are caseSensitive see the following example;
+
+let see = "This is";
+console.log(see)
+
+
+// now the same thing and just change the see to capital letterand see the result
+
+
+// let see = "This is";
+// console.log(See)
+
+
+// This will give you an error 
+
+
+
+
