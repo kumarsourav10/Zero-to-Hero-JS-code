592380bba9a2ed0537d3338db15f9251ac46d930
sourav <sauravkumarji99@gmail.com>
2022-09-21 13:34:23 +0530

    learn Binary Agents

---
+++ Intermediate Algorithm Scripting/test19.js
@@ -0,0 +1,39 @@
+// Binary Agents
+
+
+// Return an English translated sentence of the passed binary string.
+
+// The binary string will be space separated.
+
+
+//                                                                              Tests
+
+
+// Waiting:binaryAgent("01000001 01110010 01100101 01101110 00100111 01110100 00100000 01100010 01101111 01101110 01100110 01101001 01110010 01100101 01110011 00100000 01100110 01110101 01101110 00100001 00111111") should return the string Aren't bonfires fun!?
+// Waiting:binaryAgent("01001001 00100000 01101100 01101111 01110110 01100101 00100000 01000110 01110010 01100101 01100101 01000011 01101111 01100100 01100101 01000011 01100001 01101101 01110000 00100001") should return the string I love FreeCodeCamp!
+
+// this is the code
+
+/*
+
+
+function binaryAgent(str) {
+  return str;
+}
+
+binaryAgent("01000001 01110010 01100101 01101110 00100111 01110100 00100000 01100010 01101111 01101110 01100110 01101001 01110010 01100101 01110011 00100000 01100110 01110101 01101110 00100001 00111111");
+
+*/
+
+// this is the code ans
+
+function binaryAgent(str) {
+    str = str.split(" ")
+    let answer = []
+    for (let element of str){
+      answer.push(String.fromCharCode(parseInt(element, 2)))
+    }
+    return answer.join("")
+  }
+  
+  console.log(binaryAgent("01000001 01110010 01100101 01101110 00100111 01110100 00100000 01100010 01101111 01101110 01100110 01101001 01110010 01100101 01110011 00100000 01100110 01110101 01101110 00100001 00111111"));
